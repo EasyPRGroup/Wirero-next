e5fc3cb988e72a5d132cb4373c81be4de3efe088
biraj010 <regmibiraj.dev@gmail.com>
2026-07-15 14:40:24 +0545

    add: x-robots-tag

--- next.config.js
+++ next.config.js
@@ -10,6 +10,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
